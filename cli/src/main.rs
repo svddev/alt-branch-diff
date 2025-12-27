78bbc72f2533bcdf6ca657d8639361e9f4e84356
Nikita Stavtsev <stavtnr@yandex.ru>
2025-12-27 12:37:12 +0300

    cli interface implemented

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,16 +1,21 @@
-use std::collections::{HashMap, HashSet};
-
-// use altrepolib::api::export::get_branch_binary_packages;
-// use altrepolib::api::packageset::get_repository_statistics;
-use altrepolib::helpers::{get_branch_diff, get_full_list_of_architectures, get_list_of_architectures, get_list_of_branches};
+use clap::Parser;
+use altrepolib::helpers::get_branch_diff;
 
 const BRANCH_1: &str = "sisyphus";
 const BRANCH_2: &str = "p11";
 
+#[derive(Parser)]
+struct Args {
+    branch_1: String,
+    branch_2: String,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
 
-    let res = get_branch_diff(BRANCH_1, BRANCH_2).await?;
+
+    let res = get_branch_diff(&args.branch_1, &args.branch_2).await?;
 
     println!("{}", serde_json::to_string(&res)?);
     
