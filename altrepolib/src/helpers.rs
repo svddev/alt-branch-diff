bb5b32ecdfbbf84ea796a9d8997ff006014bfeff
Nikita Stavtsev <stavtnr@yandex.ru>
2025-12-27 13:15:52 +0300

    altrepolib: get_branch_diff cleanings

--- altrepolib/src/helpers.rs
+++ altrepolib/src/helpers.rs
@@ -5,7 +5,6 @@ use crate::schemas::{DiffResult, DiffResultArch, PackageInfo};
 use std::collections::{HashMap, HashSet};
 
 pub async fn get_branch_diff(branch_1: &str, branch_2: &str) -> anyhow::Result<DiffResult> {
-
     let mut result = DiffResult::new();
     
     let b1_arches = get_list_of_architectures(branch_1).await?;
@@ -18,56 +17,23 @@ pub async fn get_branch_diff(branch_1: &str, branch_2: &str) -> anyhow::Result<D
         .collect::<Vec<String>>();
 
     for arch in arches {   
-        let b1_packages = get_branch_binary_packages(branch_1, Some(arch.as_str()))
-            .await?
-            .packages
-            .into_iter()
-            .collect::<HashSet<PackageInfo>>();
-
-        let b1_packages_names = b1_packages.clone().into_iter().map(|el| {el.name}).collect::<HashSet<String>>();
-
-        let b2_packages = get_branch_binary_packages(branch_2, Some(arch.as_str()))
-            .await?
-            .packages
-            .into_iter()
-            .collect::<HashSet<PackageInfo>>();
-
-        let b2_packages_names = b2_packages.clone().into_iter().map(|el| {el.name}).collect::<HashSet<String>>();
-
-
-        let a_names = b1_packages_names
-            .difference(&b2_packages_names)
-            .cloned()
-            .collect::<HashSet<String>>();
-        
-        let a_packages = b1_packages
-            .clone()
-            .into_iter()
-            .filter(|el| {a_names.contains(&el.name)})
-            .collect::<Vec<PackageInfo>>();
-
-        let b_names = b2_packages_names
-            .difference(&b1_packages_names)
-            .cloned()
-            .collect::<HashSet<String>>();
-        
-        let b_packages = b2_packages
-            .clone()
-            .into_iter()
-            .filter(|el| {b_names.contains(&el.name)})
-            .collect::<Vec<PackageInfo>>();
-
-        let c: Vec<PackageInfo> = b1_packages
-            .into_iter()
-            .zip(b2_packages)
-            .filter(|(left, right)| { left.name == right.name && left.version > right.version })
-            .map(|el| { el.0 })
-            .collect();
-
-        result.insert(arch, DiffResultArch { additional_packages: a_packages, missing_packages: b_packages, version_greater: c });
+        let b1_packages=  get_packages(branch_1, arch.as_str()).await?;       
+        let b2_packages = get_packages(branch_2, arch.as_str()).await?; 
+        let diff_result = DiffResultArch::new(b1_packages, b2_packages);
+        result.insert(arch, diff_result);
     }
 
     Ok(result)
+}
+
+pub async fn get_packages(branch: &str, arch: &str) -> anyhow::Result<(HashSet<PackageInfo>, HashSet<String>)> {
+    let packages = get_branch_binary_packages(branch, Some(arch))
+            .await?
+            .packages
+            .into_iter()
+            .collect::<HashSet<PackageInfo>>();
+    let names = packages.clone().into_iter().map(|el| {el.name}).collect::<HashSet<String>>();
+    Ok((packages, names))
 }
 
 pub async fn get_list_of_branches() -> anyhow::Result<Vec<String>> {
--- altrepolib/src/schemas.rs
+++ altrepolib/src/schemas.rs
@@ -1,5 +1,5 @@
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub mod api;
 
@@ -30,4 +30,43 @@ pub struct DiffResultArch {
     pub additional_packages: Vec<PackageInfo>,
     pub missing_packages: Vec<PackageInfo>,
     pub version_greater: Vec<PackageInfo>
+}
+
+impl DiffResultArch {
+    pub fn new(b1_packages: (HashSet<PackageInfo>, HashSet<String>), b2_packages: (HashSet<PackageInfo>, HashSet<String>)) -> Self {
+        let a_names = b1_packages.1
+            .difference(&b2_packages.1)
+            .cloned()
+            .collect::<HashSet<String>>();
+        
+        let a_packages = b1_packages.0
+            .clone()
+            .into_iter()
+            .filter(|el| {a_names.contains(&el.name)})
+            .collect::<Vec<PackageInfo>>();
+
+        let b_names = b2_packages.1
+            .difference(&b1_packages.1)
+            .cloned()
+            .collect::<HashSet<String>>();
+        
+        let b_packages = b2_packages.0
+            .clone()
+            .into_iter()
+            .filter(|el| {b_names.contains(&el.name)})
+            .collect::<Vec<PackageInfo>>();
+
+        let c: Vec<PackageInfo> = b1_packages.0
+            .into_iter()
+            .zip(b2_packages.0)
+            .filter(|(left, right)| { left.name == right.name && left.version > right.version })
+            .map(|el| { el.0 })
+            .collect();
+
+        Self {
+            additional_packages: a_packages,
+            missing_packages: b_packages,
+            version_greater: c,
+        }
+    }
 }
